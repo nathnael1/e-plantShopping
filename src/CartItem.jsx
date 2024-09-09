5245dba18b07c8c29db33645ff07b170484b8dab
nathnael1 <nathnaeltamirat3@gmail.com>
2024-09-09 11:50:28 +0300

    configuring

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,68 +1,70 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { updateQuantity, removeItem } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
  
+    return cart.reduce((total, item) => total + item.quantity * parseFloat(item.cost.replace(/[^0-9.-]+/g,"")), 0);
+    
   };
+
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping();
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
-  // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
     <div className="cart-container">
-      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
-      <div>
+      <h2>Shopping Cart</h2>
+      <ul>
         {cart.map(item => (
-          <div className="cart-item" key={item.name}>
-            <img className="cart-item-image" src={item.image} alt={item.name} />
+          <li key={item.name} className="cart-item">
+            <img src={item.image} alt={item.name} className="cart-item-image" />
             <div className="cart-item-details">
-              <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-cost">{item.cost}</div>
+              <p className="cart-item-name">{item.name}</p>
+              <p className="cart-item-cost">Cost:{item.cost}</p>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <button onClick={() => handleIncrement(item)} className="cart-item-button">+</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                <button onClick={() => handleDecrement(item)} className="cart-item-button">-</button>
               </div>
-              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+              <p className="cart-item-total">Subtotal: ${item.quantity *  parseFloat(item.cost.replace(/[^0-9.-]+/g,""))}</p>
+              <button onClick={() => handleRemove(item)} className="cart-item-delete">Remove</button>
             </div>
-          </div>
+          </li>
         ))}
-      </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
-      <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
-        <br />
-        <button className="get-started-button1">Checkout</button>
-      </div>
+      </ul>
+      <h3 className="total_cart_amount">Total Amount: ${calculateTotalAmount()}</h3>
+      <button onClick={handleContinueShopping} className="get-started-button1">Continue Shopping</button>
+      <button onClick={handleCheckoutShopping} className="get-started-button1">Checkout</button>
     </div>
   );
 };
 
 export default CartItem;
-
-
